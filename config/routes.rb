d287e55562c52e4d8dff387248641923a2bc7ad2
tarokuma3 <rodysan.mojya@gmail.com>
2018-12-02 20:03:20 +0900

    微調整
    ・トップの追加
    ・成果物登録のフォームを途中まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get "/" => 'users#index'
+  get "/" => 'top#index'
   get '/users/:user_id/mokus/:id/new' => 'work#new'
   post '/users/:user_id/mokus/:id/create' => 'work#create'
 

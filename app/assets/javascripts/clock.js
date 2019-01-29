42b14d235190f7df6a8f2f662bdb59feb3de487a
tarokuma3 <rodysan.mojya@gmail.com>
2019-01-29 10:01:24 +0900

    JSのエラー修正とpartial化

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,18 +1,3 @@
-'use strict';
 
-var time;
-      var hour = document.getElementById("hour");
-      var minute = document.getElementById("minute");
-      var second = document.getElementById("second");
 
-      function main() {
-        time = new Date();
 
-        hour.style.transform = "rotate("+(time.getHours()*30+time.getMinutes()*0.5)+"deg)";
-        minute.style.transform = "rotate("+(time.getMinutes()*6)+"deg)";
-        second.style.transform = "rotate("+(time.getSeconds()*6)+"deg)";
-
-        setTimeout(main, 1000-time.getMilliseconds());
-      }
-
-      main();

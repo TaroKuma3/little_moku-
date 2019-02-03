d770e709465572fead9ff82b9afbd52c6d02242d
tarokuma3 <rodysan.mojya@gmail.com>
2019-02-03 16:38:39 +0900

    moku.rbのバリデーション
    １：通常バリデーションでstarted_at, presence: trueが抜けてた
    
    ２：on: :updateオプションでfinished_atとmoku_timeを追加

--- app/models/moku.rb
+++ app/models/moku.rb
@@ -2,13 +2,17 @@ class Moku < ApplicationRecord
   belongs_to :user
   belongs_to :moku_type
   has_many :works
-  has_many :stamps
+  has_many :stamps　#使わない
 
-  # 終了時と中断時にも受け取るべきデーターがある。それらは今設定すると動かなくなるので設定してない
-
+  #create時からずっと
   validates :user_id, presence: true
   validates :moku_type_id, presence: true
   validates :mjn_public, inclusion: { in: [true, false] }
+  validates :started_at, presence: true
+
+  #update == MOKU終了時以降
+  validates :finished_at, presence: true, on: :update
+  validates :moku_time, presence: true, on: :update
 
   def format_created_at
     self.created_at.strftime('%y/%m/%d  %H:%M')

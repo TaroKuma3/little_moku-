02311983a70cd76bbfeac4a0e8f4b4914638941d
tarokuma3 <rodysan.mojya@gmail.com>
2019-02-05 14:00:50 +0900

    微調整

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   belongs_to :moku
   belongs_to :user
-  has_many :stamp
+  has_many :stamp #使わない
   has_many :book_marks
 
   has_many_attached :images
@@ -9,6 +9,6 @@ class Work < ApplicationRecord
   validates :moku_id, presence: true
   validates :user_id, presence: true
   validates :title, presence: true
-  validates :comment_public, presence: true #booleanじゃなくてintegerだから
-  validates :pick_up, presence: true #booleanじゃなくてintegerだから
+  validates :comment_public, presence: true #booleanじゃなくてintegerだからpresence: true
+  validates :pick_up, presence: true #booleanじゃなくてintegerだからpresence: true
 end
